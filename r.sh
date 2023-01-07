b0cabfd441371462cd4210dbcc8f328b6454c66f
Adam Kiss <iam@adamkiss.com>
2023-01-07 23:04:34 +0100

    Implement the build scripts

---
+++ r.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env zsh
+# fake npm scripts hahaha
+
+about () { #: show help & commands
+    NAME='alfred-noteplan-fts'
+    echo "$NAME script runner"
+    echo "Commands:"
+    cat r.sh | sed -nr 's/^(.*) \(\).* #: (.*)$/  \1\t\2/p' | expand -20
+}
+
+build:dart () { #: Build the version for the current architecture
+    dart compile exe src/bin/refresh.dart -o "workflow/refresh-$(uname -m)"
+}
+
+build:icons () { #: Build the icns file from iconsets
+    echo "not implemented yet."
+}
+
+build:workflow () { #: Zip the workflow folder into release/dist folder
+    echo "not implemented yet."
+}
+
+build () { #: Run the whole build
+    build:icons
+    build:dart
+    build:workflow
+}
+
+if [[ $# > 0 ]]; then
+    script=`shift 1`
+    $script "$@"
+else
+    about
+fi
