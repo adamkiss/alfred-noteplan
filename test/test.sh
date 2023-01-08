c741c555fa426caa3cb10b4ac7acfdd451bdfca4
Adam Kiss <iam@adamkiss.com>
2023-01-09 00:05:08 +0100

    Prepare tests

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env zsh
 
 # CWD is the root
+echo "RETURN HELP OPTIONS"
 zsh test/fake-run.sh !
+echo
+echo
+
+echo "SHOW DEBUG INFORMATION"
 zsh test/fake-run.sh !!
-zsh test/fake-run.sh !r
+echo
+echo
+
+echo "FORCE REFRESH"
 zsh test/fake-run.sh !rf
+echo
+echo
+
+echo "REFRESH CHANGED (nothing)"
+zsh test/fake-run.sh !r
+echo
+echo
