9d5eb4481d6988a3c01ac7a4c11bf45c0c325225
Adam Kiss <iam@adamkiss.com>
2023-01-09 03:35:53 +0100

    enhance NoteType enum with private getters

--- lib/note_type.dart
+++ lib/note_type.dart
@@ -1,8 +1,26 @@
 enum NoteType {
-	note,
-	daily,
-	weekly,
-	monthly,
-	quarterly,
-	yearly
+	note      ,
+	daily     ,
+	weekly    ,
+	monthly   ,
+	quarterly ,
+	yearly    ;
+
+	String get value {
+		switch (this) {
+			case NoteType.note: return 'note';
+			case NoteType.daily: return 'daily';
+			case NoteType.weekly: return 'weekly';
+			case NoteType.monthly: return 'monthly';
+			case NoteType.quarterly: return 'quarterly';
+			case NoteType.yearly: return 'yearly';
+			default: return ''; // won't happen
+		}
+	}
+
+	String get np_folder {
+		return this == NoteType.note
+			? 'Notes'
+			: 'Calendar';
+	}
 }
