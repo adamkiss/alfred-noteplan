7ca1f3444b5e83e46dd1c86e4bf51299bd48a739
Adam Kiss <iam@adamkiss.com>
2023-01-09 03:35:34 +0100

    try to put strings in one file for control / update

---
+++ lib/strings.dart
@@ -0,0 +1,4 @@
+// ignore_for_file: constant_identifier_names
+
+const str_fts_result_arg_cmd_subtitle = 'Open the note in a new Noteplan window';
+const str_create_result_subtitle = 'Create a new note ✱ You''ll be asked for location in the next step';
